24dc2e4bf2e3ae5d105660571aedd14faa76992f
panthitesh6410 <panthitesh6410@gmail.com>
2020-02-01 20:59:37 +0530

    message or feedback functionality added

--- website/main_app/views.py
+++ website/main_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from . models import enquiry
 
 # Create your views here.
 
 def index(request):
-    return render(request,'main_app/index.html')
+    if request.method =='POST':
+        user_name = request.POST['name']
+        user_email = request.POST['email']
+        user_message = request.POST['message']
+        e = enquiry(name=user_name, email=user_email, message=user_message)
+        e.save()
+        message = {'message':'Your Message has been sent successfully '}
+        return render(request,'main_app/index.html', {'message':message})
+    else:
+        return render(request,'main_app/index.html')
